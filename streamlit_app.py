3a59de4ae26a28a5b9539f18d159427829c1ef87
lpkkelompok10 <tugaslpkkelompok@gmail.com>
2026-06-06 11:07:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -700,8 +700,10 @@ elif menu == "🧪 Stoikiometri":
     with col2:
         Mr = st.number_input("Mr zat", min_value=0.0)
         
-        if st.button("Hitung Mol"):
+       if st.button("Hitung Mol"):
+
     if Mr > 0:
+
         with st.spinner("🧪 Menghitung..."):
             time.sleep(1)
 

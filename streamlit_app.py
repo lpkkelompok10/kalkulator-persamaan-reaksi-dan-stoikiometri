44fb93ddf2da66655bc59d30e11dda85650650f0
lpkkelompok10 <tugaslpkkelompok@gmail.com>
2026-06-15 07:42:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,8 @@ team_anim = load_lottie(
     "https://assets1.lottiefiles.com/packages/lf20_tutvdkg0.json"
 )
 
-
+with st.spinner("⚗️ Memuat aplikasi..."):
+    time.sleep(0.2)
 
 # =========================
 # STYLE
